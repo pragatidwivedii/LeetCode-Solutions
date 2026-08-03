6bdb81ba8652c9ea0d75603f2355f31a7c63db31
Pragati Dwivedi <153013169+pragatidwivedii@users.noreply.github.com>
2026-08-03 15:29:50 +0530

    Time: 0 ms (100%), Space: 16.6 MB (98.03%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     string largestOddNumber(string num) {
       int n = num.size();
-    //   cout<<((num[n-1]-'0')&1);
-    //   num.erase(n-1,1);
-    //   cout<<num;
       for(int i = n-1 ; i >= 0 ; i--){
         if(((num[i]-'0')&1) == 0){
             num.erase(i,1);
